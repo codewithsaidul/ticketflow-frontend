db07ba655f622dd416854e3e9e8408c5de760e0e
codewithsaidul <codewithsaidul@gmail.com>
2026-05-03 12:31:08 +0600

    url updated

--- next.config.ts
+++ next.config.ts
@@ -14,22 +14,22 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "**", 
+        hostname: "**",
       },
     ],
   },
 
-    async rewrites() {
+  async rewrites() {
     const isDev = process.env.NODE_ENV === "development";
 
-    const backendUrl = isDev 
-      ? process.env.NEXT_PUBLIC_DEV_BACKEND_URL 
+    const backendUrl = isDev
+      ? process.env.NEXT_PUBLIC_DEV_BACKEND_URL
       : process.env.NEXT_PUBLIC_PROD_BACKEND_URL;
 
     return [
       {
         source: "/api/v1/:path*",
-        destination: `${backendUrl}/:path*`, 
+        destination: `${backendUrl}/:path*`,
       },
     ];
   },
--- src/components/modules/auth/GoogleLogin.tsx
+++ src/components/modules/auth/GoogleLogin.tsx
@@ -3,12 +3,12 @@ import { GoogleIcon } from "@/components/icon/google-icon";
 import { Button } from "@/components/ui/button";
 import { useSearchParams } from "next/navigation";
 
-const isDev = process.env.NODE_ENV === "development";
-const backendUrl = isDev
-  ? process.env.NEXT_PUBLIC_DEV_BACKEND_URL
-  : process.env.NEXT_PUBLIC_PROD_BACKEND_URL;
+// const isDev = process.env.NODE_ENV === "development";
+// const backendUrl = isDev
+//   ? process.env.NEXT_PUBLIC_DEV_BACKEND_URL
+//   : process.env.NEXT_PUBLIC_PROD_BACKEND_URL;
 
-
+const backendUrl = "/api/v1";
 
 export default function GoogleLogin ( { isLoading }: { isLoading: boolean }) {
   const searchParams = useSearchParams();
